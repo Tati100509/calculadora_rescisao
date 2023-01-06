37afbb6e3562807148da82c8a43c928a15db1ac2
Tati100509 <tatianecolenascimento@gmail.com>
2023-01-06 09:48:38 -0300

    initial commit

--- index.js
+++
@@ -1,9 +0,0 @@
-function calculaFeriasProporcionais(meses, valor) {
-    let feriasProporcionais = (valor/12) * meses 
-    return feriasProporcionais.toFixed(2)
-
-}
-
-let meses = 12
-let valor = 1760
-console.log(calculaFeriasProporcionais(12, 1760))
--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 function calculaSaldoSalario(salario,dias){
     let saldoSalario = (salario/30) *  dias
-    return saldoSalario
+    return saldoSalario.toFixed(2)
 
 }
 
 let salario = 1320
 let dias = 26
 console.log(calculaSaldoSalario(1320,26))
+
+
+function calculaFeriasProporcionais(meses, valor) {
+    let feriasProporcionais = (valor/12) * meses 
+    return feriasProporcionais.toFixed(2)
+
+}
+
+let meses = 12
+let valor = 1760
+console.log(calculaFeriasProporcionais(12, 1760))
